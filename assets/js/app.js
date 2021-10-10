6f5d9adebc2c115b7cc8d6b81676f54c8c059602
Alissa Nordmoe <alissa.nordmoe@gmail.com>
2021-10-10 15:02:21 -0400

    added bootstrap & personal style

---
+++ assets/js/app.js
@@ -0,0 +1,73 @@
+console.log("hello world");
+
+let exampleVariable = 13;
+
+console.log(exampleVariable);
+	
+// let amountRaisedSoFar = 1000;
+ 
+// let donation = prompt("How much would you like to donate?"); 
+ 
+// amountRaisedSoFar = Number(donation) + amountRaisedSoFar;
+ 
+// console.log("We have now raised: " + amountRaisedSoFar + "!");
+
+let temp = 80;
+let indoors = true;
+
+console.log(`The temperature is ${temp} degrees.`);
+
+if (temp > 80 ) {
+    console.log("Time to swim!");
+}
+
+let precipitation = false;
+
+if (temp > 80 && precipitation === false) {
+    console.log("Time to swim!");
+}
+
+precipitation = "raining";
+
+if (indoors) {
+    console.log("time to swim")
+}
+
+	
+// let temp = 85;
+// let precipitation = false;
+// let indoors = true;
+ 
+// console.log("The temperature is " + temp + " degrees");
+ 
+// if (temp > 80 && precipitation === false) {
+//   console.log("time to swim!");
+// } else if (indoors) {
+//   console.log("time to swim!");
+// }
+
+// for (let i = 99; i >= 0; i--) {
+//     if(i === 0){
+//         console.log("Hey! We need more root beer!")
+//     } else {
+//     console.log(`${i} bottles of beer on the wall / take one down & pass it around`)
+//     }
+// }
+
+// let x = 1;
+// switch(x){
+//     case 3: 
+//         console.log(3);
+//     case 1: 
+//         console.log(1);
+//     case 2:
+//         console.log(2);
+//         break;
+//     default: 
+//         console.log()
+// }; 
+	
+for (var i = 0; i < 10; i++){
+    console.log(i); //
+  }
+  console.log(i); 
